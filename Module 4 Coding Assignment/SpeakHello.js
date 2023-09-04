5b0936921b041cb73b157baa546cdda8b67365f8
Marthe1983 <139658674+Marthe1983@users.noreply.github.com>
2023-09-03 22:01:37 -0600

    Update SpeakHello.js

--- Module 4 Coding Assignment/SpeakHello.js
+++ Module 4 Coding Assignment/SpeakHello.js
@@ -21,4 +21,4 @@ function helloSpeaker.speak(names) {
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-helloSpeaker.speak = helloSpeaker;
+helloSpeaker.names = helloSpeaker;
